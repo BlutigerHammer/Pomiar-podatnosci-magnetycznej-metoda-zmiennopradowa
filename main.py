5b7132b65c2272840d0941a25435198d4ffee245
Eryk Hammer <erykhammer@wp.pl>
2020-08-09 11:27:59 +0200

    v 0.1 - program measure and save data to txt file

---
+++ main.py
@@ -0,0 +1,40 @@
+import serial
+import time
+import find_com
+import pyvisa
+
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+('ASRL1::INSTR', 'ASRL2::INSTR', 'GPIB0::12::INSTR')
+thermometer = rm.open_resource('USB0::0x164E::0x0DAD::TW00020217::INSTR')
+# print(thermometer.query("*IDN?"))
+
+com = 'COM10'
+voltmeter = serial.Serial(com, baudrate=9600, timeout=1)
+time.sleep(2)
+
+voltmeter.write(b'P 0.0 \r')
+
+with open ('dane.txt', 'w') as f:
+    f.write('Temp:\tU:\n')
+    for i in range(5):
+        print(i)
+        t = thermometer.query("MEAS:VOLT:DC?")
+        t = str(t)
+        t += '\t'
+        print('temp:', t)
+        f.write(t)
+
+        voltmeter.write(b' Q \r')
+        u = voltmeter.readline()
+        u = str(u)
+        u = u[2:-3]
+        u += '\n'
+        print('U', u)
+        f.write(u)
+        #time.sleep(1)
+
+
+f.close()
+serial.Serial.close(voltmeter)
+thermometer.close()
