868ec10ccc4020f1258e5d5d33ff2fcbc3f43112
Eryk Hammer <erykhammer@wp.pl>
2020-08-31 18:54:15 +0200

    add label which shows current values, add save file dialog, addconstant BACKGROUND_MAGNETISM

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, QtCore, uic
+from PyQt5 import QtWidgets, QtCore, uic, QtGui
 from pathlib import Path
 import sys
 from main import Aparature
@@ -7,6 +7,7 @@ from time import sleep
 
 
 class MainWindow(QtWidgets.QMainWindow):
+    BACKGROUND_MAGNETISM = 1.45e-5
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
@@ -16,12 +17,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.x = []
         self.y = []
         self.data = None
-        self.folder_name = None
-        self.file_name = 0
-        self.phase = 0
+        self.phase = -90
         self.data_function = None
 
         self.plotWidget.setBackground('w')
+
 
         self.data_line = self.plotWidget.plot(self.x, self.y, symbol='o', symbolSize=5, symbolBrush='k', symbolPen=None,
                                               pen=None)
@@ -37,59 +37,66 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.Meters = Aparature()
         self.data_function = self.Meters.measurement()
-        save_to.create_xmls(Path('data/excel-28-08-20.xlsx'))
+        # save_to.create_xmls(Path('data/excel-31-08-20.xlsx'))
 
     def sample_setting_plot(self):
         self.data = self.Meters.sample_setting()
         if self.data is not None:
             self.x.append(self.data[0])
-            self.y.append(self.data[1])
+            self.y.append(self.data[1] - BACKGROUND_MAGNETISM)
             self.data_line.setData(self.x, self.y)  # Update the data.
 
+
     def phase_setting_plot(self):
-        self.phase = 90
+        self.phase = - 90
         self.Meters.voltmeter.write(bytes(f"P {self.phase} \r", encoding='utf8'))
         sleep(5)
-        for i in range(90, 100):
-            self.data = self.Meters.phase_setting(i)
+        for phase in range(90, 100):
+            self.data = self.Meters.phase_setting(-phase)
             if self.data is not None:
                 self.x.append(self.data[0])
-                self.y.append(self.data[1])
+                self.y.append(self.data[1] - BACKGROUND_MAGNETISM)
                 self.data_line.setData(self.x, self.y)  # Update the data.
         self.phase = self.x[self.y.index(max(self.y))]
         print(self.phase)
-        self.stopButton_clicked(False)
+        self.stopButton_clicked()
 
     def measurement_plot(self):
         self.data = self.Meters.measurement()
         if self.data is not None:
             # print(self.data)
             self.x.append(self.data[0])
-            self.y.append(self.data[1])
+            self.y.append(self.data[1] - BACKGROUND_MAGNETISM)
             self.data_line.setData(self.x, self.y)  # Update the data.
+            self.xValue.setText(str(round(self.data[0], 1)))
+            self.yValue.setText(str(self.data[1]))
+
 
     def sampleSetting_clicked(self):
         self.data_function = self.sample_setting_plot
         self.startButton.setEnabled(True)
         self.plotWidget.setTitle("Ustawienie próbki", color='k')
-        self.plotWidget.setLabel('left', 'Napięcie [V]')
-        self.plotWidget.setLabel('bottom', 'Położenie')
+        self.plotWidget.setLabel('left', 'Napięcie [V]', color='k')
+        self.plotWidget.setLabel('bottom', 'Położenie', color='k')
 
     def phaseSetting_clicked(self):
         self.data_function = self.phase_setting_plot
         self.startButton.setEnabled(True)
         self.plotWidget.setTitle("Ustawienie fazy", color='k')
-        self.plotWidget.setLabel('left', 'Napięcie [V]')
-        self.plotWidget.setLabel('bottom', 'Faza [°]')
+        self.plotWidget.setLabel('left', 'Napięcie [V]', color='k')
+        self.plotWidget.setLabel('bottom', 'Faza [°]', color='k')
+        self.xLabel.setText('Faza [°]')
+        self.yLabel.setText('Napięcie [V]')
 
     def measurement_clicked(self):
         self.data_function = self.measurement_plot
         self.startButton.setEnabled(True)
         self.plotWidget.setTitle("Pomiar", color='k')
-        self.plotWidget.setLabel('left', 'Napięcie [V]')
-        self.plotWidget.setLabel('bottom', 'Temperatura [K]')
+        self.plotWidget.setLabel('left', 'Napięcie [V]', color='k')
+        self.plotWidget.setLabel('bottom', 'Temperatura [K]', color='k')
         self.Meters.voltmeter.write(bytes(f"P {-self.phase} \r", encoding='utf8'))
-
+        self.xLabel.setText('Temperatura [K]')
+        self.yLabel.setText('Napięcie [V]')
 
     def startButton_clicked(self):
         self.startButton.setEnabled(False)
@@ -104,8 +111,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.data_function)
         self.timer.start()
 
-
-    def stopButton_clicked(self, save=True):
+    def stopButton_clicked(self):
         self.stopButton.setEnabled(False)
         self.timer.timeout.disconnect(self.data_function)
         self.startButton.setEnabled(True)
@@ -118,14 +124,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.x = []
         self.y = []
 
-
     def save_data(self):
-        path = Path('data/excel-28-08-20.xlsx')
-        sheet_name = 'a' + str(self.file_name)
-        save_to.save_to_xmls(path, self.x, self.y, sheet_name)
-        path = Path(f'data/data-28-08-20_{self.file_name}.csv')
+        # path = Path('data/excel-31-08-20.xlsx')
+        # sheet_name = 'a' + str(self.file_name)
+        # save_to.save_to_xmls(path, self.x, self.y, sheet_name)
+        path, _ = QtWidgets.QFileDialog.getSaveFileName(self, "", "", "CSV Files (*.csv) ;;Text Files (*.txt)")
         save_to.save_to_csv(path, self.x, self.y)
-
 
 
 app = QtWidgets.QApplication(sys.argv)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pyvisa
 
 
 class Aparature:
+
     def __init__(self):
         rm = pyvisa.ResourceManager()
         rm.list_resources()
@@ -15,7 +16,7 @@ class Aparature:
         self.voltmeter = serial.Serial(com, baudrate=9600, timeout=1)
         time.sleep(2)
 
-        self.voltmeter.write(b'P 90 \r')
+        self.voltmeter.write(b'P -90 \r')
 
     def sample_setting(self):
         position = 0
@@ -35,6 +36,7 @@ class Aparature:
         t = voltage_to_kelvins(float(t))
         self.voltmeter.write(b' Q \r')
         u = self.voltmeter.readline().decode()
+        u = float(u)
 
         return [t, u]
 
@@ -48,7 +50,8 @@ class Aparature:
         serial.Serial.close(self.voltmeter)
         self.thermometer.close()
 
+
 def voltage_to_kelvins(x):
-    x *= 1000
-    x += 1
-    return 272.99294 + 25.83372*x - 0.78202*x**2 + 0.23669*x**3 + 0.07095*x**4 + 0.01113*x**5
+    x *= 1000  # volts to milivolts
+    x += 1  # Cold Junction Compensation
+    return 272.99294 + 25.83372 * x - 0.78202 * x ** 2 + 0.23669 * x ** 3 + 0.07095 * x ** 4 + 0.01113 * x ** 5
